d209cc15098d26fb9703a02bf0a648e298398029
Josimari Fabri <josimari.fabri@gmail.com>
2024-03-04 14:41:59 -0300

    alterações com comandos customizados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -40,13 +40,18 @@ describe('Funcionalidade: Login', () => {
 
     });
 
-    it.only('Deve fazer login com sucesso usando fixture', () => {
+    it('Deve fazer login com sucesso usando fixture', () => {
         cy.fixture('perfil').then( dados => {
             cy.get('#username').type(dados.usuario, {log: false})
             cy.get('#password').type(dados.senha, { log: false })
             cy.get('.woocommerce-form > .button').click()
             cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, josimari.teste (não é josimari.teste? Sair)')    
-        })
+        })    
     });
+
+    it('Deve fazer login com sucesso usando Comandos Costumizados', () => {
+        cy.login('josimari.teste@teste.com.br', 'teste@123')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, josimari.teste (não é josimari.teste? Sair)')
+    })
 
 })
