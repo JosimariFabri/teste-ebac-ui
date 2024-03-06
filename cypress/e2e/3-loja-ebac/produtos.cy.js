36220232bd7da7fdf51f4fcc7f629749f7dcda01
Josimari Fabri <josimari.fabri@gmail.com>
2024-03-06 16:01:17 -0300

    alteração usando page object e massa de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,20 +1,47 @@
 ///<reference types="cypress"/>
+import produtosPage from "../../support/page.objects/produtos.page";
 
 describe('Funcionalidade: produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Apollo Running Short')
-            .click()
-
+        produtosPage.buscarProdutoLista('Aether Gym Pant')
             cy.get('#tab-title-description > a').should('contain', 'Descrição')
+        
+    });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Abominable Hoodie'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a página de um produto', () => {
+        produtosPage.visitarProduto('Aero Daily Fitness Tee')
+        cy.get('.product_title').should('contain', 'Aero Daily Fitness Tee')
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Arcadio Gym Short')
+        produtosPage.addProdutoCarrinho('33', 'Blue', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Arcadio Gym Short” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho, 
+                dados[0].cor, 
+                dados[0].quantidade)
+                cy.get('.product_title').should('contain', dados[0].nomeProduto)
+
+        })
+
         
     });
 });
